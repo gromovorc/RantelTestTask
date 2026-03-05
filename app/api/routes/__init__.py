dd93b9814b73ed6383f9a6c6ea097e751d841f91
gromovorc <72044466+gromovorc@users.noreply.github.com>
2026-03-05 22:16:52 +0300

    messages route

--- app/api/routes/__init__.py
+++ app/api/routes/__init__.py
@@ -1,6 +1,7 @@
 from aiohttp import web
 
 from .clients import clients_routes
+from .messages import messages_routes
 from .operators import operators_routes
 from .tickets import tickets_routes
 from .dashboard import dashboard_routes
@@ -10,3 +11,4 @@ def setup_routes(app: web.Application) -> None:
     app.add_routes(operators_routes)
     app.add_routes(tickets_routes)
     app.add_routes(dashboard_routes)
+    app.add_routes(messages_routes)
